b0b6343e7878a0f3154fc6a4ebb44c1c5eb642f0
YChoi <68574132+CrI3@users.noreply.github.com>
2024-07-24 13:12:53 -0500

    reading txt file

--- test_qt2.py
+++ test_qt2.py
@@ -8,6 +8,9 @@ from PyQt5.QtWidgets import (QApplication, QMainWindow,
                              )
 
 import test_matplot  # import test_maplot.py to use its function
+#added 7/23
+import numpy as np
+import matplotlib.pyplot as plt
 
 '''
 YC, 7/11
@@ -22,7 +25,7 @@ class MainWindow(QMainWindow):
         layout = QVBoxLayout()
         
         #   add text line
-        text1 = QLabel('Object-1')
+        text1 = QLabel('Plot type')
         self.aCombo = QComboBox()
         self.aCombo.addItems(['Linear', 'Exponential'])
 
@@ -31,11 +34,11 @@ class MainWindow(QMainWindow):
         self.aLineText = QLineEdit('10')
         
         # add checkbox 
-        text3 = QLabel('Object-3')
-        self.aCheckBox = QCheckBox('Option?')
+        text3 = QLabel('Add marker in plot')
+        self.aCheckBox = QCheckBox('Marker?')
 
         #   add button
-        text4 = QLabel('Object-4')
+        text4 = QLabel('Button')
         self.aButton = QPushButton("Press Here!")
         self.aButton.setCheckable(True)
         # now connect object and function, when clicked
@@ -46,13 +49,13 @@ class MainWindow(QMainWindow):
 
         # add to widgets for layout
         widgets = [
-            text2,
-            self.aCombo,
             text1,
+            self.aCombo,
+            text2,
             self.aLineText,
-            text4,
-            self.aCheckBox,
             text3,
+            self.aCheckBox,
+            text4,
             self.aButton,
             self.textOut
         ]
@@ -74,16 +77,31 @@ class MainWindow(QMainWindow):
         aaInd = self.aCombo.currentIndex()  # return selected index, 0,1...
         aaInd = str(aaInd)                  # make integer to string
         bb = self.aLineText.text()          # read text from line text
-        cc = self.aCheckBox.checkState()  # off/on = 0/2 
+        use_marker = self.aCheckBox.checkState()  # off/on = 0/2 
         # print out to the command line
-        output = aaType + aaInd + ', ' + bb + ', ' + str(cc)
+        output = aaType + aaInd + ', ' + bb + ', ' + str(use_marker)
         print (output)
         # display in textOut
         self.textOut.setText(output)
         self.textOut.setStyleSheet('background-color:cyan')  # change color
         # call a funtion in another py file and send option.
-        test_matplot.plot_xy(aaType, int(bb))  
+        #test_matplot.plot_xy(aaType, int(bb), use_marker)  
+        # test loadtxt
+        infile = 'simSpectrum_plot.txt'
+        xx, yy = np.loadtxt (infile, unpack=True)  # "import numpy as np" added at the beginning of the file.
+        # now xx, yy are arrays with x and y values.  
+        print ('number of points = ', len(xx), len(yy))
+        plot_xy2(xx,yy, use_marker)
 
+# make a plot using matplotlib
+def plot_xy2(xx, yy, use_marker):
+    if use_marker != 0:
+        plt.plot(xx, yy, marker='o')   # use marker in the plot   
+    else:
+        plt.plot(xx, yy)                # no marker.
+    plt.ylabel('y-values')
+    plt.xlabel('x-values')
+    plt.show()
 
 # make a window with objects defined above
 app = QApplication(sys.argv)
